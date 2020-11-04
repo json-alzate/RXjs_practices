aee88b320db48ea558381e7e69867ab10cf26ffc
Jheison Alzate <developer.alzate@gmail.com>
2020-11-03 20:30:47 -0500

    :tada: back to play

--- curso-rxjs-inicio/src/index.ts
+++ curso-rxjs-inicio/src/index.ts
@@ -1,8 +1,18 @@
+import { Observable } from 'rxjs';
 
+const obj$ = new Observable<string>(subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+    subscriber.complete();
+});
 
-console.log('Hola Mundo!');
+obj$.subscribe(console.log);
 
-
+obj$.subscribe(
+    resp => console.log('next ', resp),
+    error => console.log('Error ', error),
+    () => console.log('complete')  
+);
 
 
 
